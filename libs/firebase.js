25a49982694186332e9ed92e2bded6a314bf01a7
Alejandro <alejandrom.am33@gmail.com>
2022-09-02 01:17:07 -0500

    add: connection to firebase

---
+++ libs/firebase.js
@@ -0,0 +1,16 @@
+import {initializeApp, getApps, getApp} from 'firebase/app'
+import {getFirestore} from "firebase/firestore"
+
+let app 
+if(!getApps.length){
+    app= initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        appId: process.env.NEXT_PUBLIC_APP_ID,
+    })
+} else {
+    app = getApp()
+}
+
+export const database = getFirestore(app)
