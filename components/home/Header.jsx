615c80bb76470fa1b8b8cdee2d7f8c1585ff319b
Alejandro <alejandrom.am33@gmail.com>
2022-09-03 01:10:54 -0500

    add: header in mobile

---
+++ components/home/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import greenFigure from "../../assets/greenFigure.png"
+
+export default function Header() {
+    return (
+        <div className='header-container'>
+            <div className='header-image-container'>
+                <Image 
+                    className='header-image'
+                    src={greenFigure} 
+                    alt='green figure'
+                />
+            </div>
+            <div className='header-greeting-container'>
+                <h1>Buenos dias, Alejandro</h1>
+                <p>Explora los productos que tenemos para ti</p>
+            </div>
+        </div>
+    )
+}
