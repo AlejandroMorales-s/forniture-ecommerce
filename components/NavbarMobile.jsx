34750f74fe4f8e0c8ae1772d5baca53521904b3e
Alejandro <alejandrom.am33@gmail.com>
2022-09-04 18:47:29 -0500

    add: logic in navbar icons when they are active

--- components/NavbarMobile.jsx
+++ components/NavbarMobile.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
 import {BiStore} from 'react-icons/bi'
 import {MdFavoriteBorder} from 'react-icons/md'
 import {FiShoppingCart} from 'react-icons/fi'
 import profileImage from '../assets/profileImage.jpg'
 
 export default function NavbarMobile() {
+    const router = useRouter()
     return (
-        <div className='navbar-mobile-container'>
-            <BiStore className='navbar-icon'/>
-            <MdFavoriteBorder className='navbar-icon'/>
-            <FiShoppingCart className='navbar-icon'/>
+        <nav className='navbar-mobile-container'>
+            <Link href={'/home'}>
+                <BiStore className={`${router.pathname == "/home" && "active-navbar-icon"} navbar-icon`}/>
+            </Link>
+            <Link href={'/favorites'}>
+                <MdFavoriteBorder className={`${router.pathname == "/favorites" && "active-navbar-icon"} navbar-icon`}/>
+            </Link>
+            <Link href={'/shopping-cart'}>
+                <FiShoppingCart className={`${router.pathname == "/shopping-cart" && "active-navbar-icon"} navbar-icon`}/>
+            </Link>
             <div className='profile-navbar-container'>
                 <div>
                     <Image 
@@ -19,6 +28,6 @@ export default function NavbarMobile() {
                     />
                 </div>
             </div>
-        </div>
+        </nav>
     )
 }
