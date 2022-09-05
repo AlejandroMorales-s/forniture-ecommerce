d625053003443cd2937dba11d07aabf26a82b161
Alejandro <alejandrom.am33@gmail.com>
2022-09-05 01:07:58 -0500

    add: categories

---
+++ components/home/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Categories() {
+    return (
+        <div className='categories-container'>
+            <p className='category'>Descubre</p>
+            <p className='category'>Sillas</p>
+            <p className='category'>Escritorios</p>
+            <p className='category'>Sillónes</p>
+            <p className='category'>Repisas</p>
+            <p className='category'>Mesas de noche</p>
+        </div>
+    )
+}
