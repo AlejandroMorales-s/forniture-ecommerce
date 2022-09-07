0f90cf43fdde5f5459f5261665f180a60a8bf5c5
Alejandro <alejandrom.am33@gmail.com>
2022-09-07 15:16:12 -0500

    add: menu in navbar

--- components/NavbarMobile.jsx
+++ components/NavbarMobile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
@@ -6,9 +6,22 @@ import {BiStore} from 'react-icons/bi'
 import {MdFavoriteBorder} from 'react-icons/md'
 import {FiShoppingCart} from 'react-icons/fi'
 import profileImage from '../assets/profileImage.jpg'
+import { signOut } from 'firebase/auth'
+import { auth } from '../libs/firebase'
+import { authContext } from '../context/AuthContext'
 
 export default function NavbarMobile() {
     const router = useRouter()
+    const {setUser} = useContext(authContext)
+    const [menuActive, setMenuActive] = useState(false)
+
+    const logout = () => {
+        signOut(auth)
+        setUser({
+            logout: true
+        })
+        router.push('/')
+    }
     return (
         <nav className='navbar-mobile-container'>
             <Link href={'/home'}>
@@ -20,13 +33,16 @@ export default function NavbarMobile() {
             <Link href={'/shopping-cart'}>
                 <FiShoppingCart className={`${router.pathname == "/shopping-cart" && "active-navbar-icon"} navbar-icon`}/>
             </Link>
-            <div className='profile-navbar-container'>
+            <div onClick={() => setMenuActive(!menuActive)} className='profile-navbar-container'>
                 <div>
                     <Image 
                         alt='profile'
                         src={profileImage}
                     />
                 </div>
+            </div>
+            <div className={`${!menuActive && 'inactive'} navbar-mobile-menu`}>
+                <button className='logout-button' onClick={logout}>Cerrar Sesión</button>
             </div>
         </nav>
     )
