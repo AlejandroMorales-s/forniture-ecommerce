9da169b067cd3bb327883cc9a369043e67067171
Alejandro <alejandrom.am33@gmail.com>
2022-09-12 12:43:41 -0500

    add: greeting in function the current hour

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -1,28 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Image from 'next/image'
 import { authContext } from '../../context/AuthContext'
-import defaulImage from '../../assets/userDefault.png'
+import defaultImage from '../../assets/userDefault.png'
 
 export default function Header() {
+    const [currentTime, setCurrentTime] = useState('')
+
     const {user} = useContext(authContext)
     const {name, photo} = user
+    
+    let actualHour = new Date()
+    
+    useEffect(() => {
+        if(actualHour.getHours() >= 2 && actualHour.getHours() < 12) setCurrentTime('Buenos dias')
+        else if(actualHour.getHours() >= 12 && actualHour.getHours() < 20) setCurrentTime('Buenas tardes')
+        else setCurrentTime('Buenas noches')
+    }, [])
     return (
         <header className='header-container'>
             <h1 className='logo'>Forniture.</h1>
             <div className='header-info-container'>
                 <div>
-                    <h2>Buenos dias, {name}</h2>
+                    <h2>{currentTime}, {name}</h2>
                     <p>Explora los mejores muebles</p>
                 </div>
                 <div className='header-image-container'>
                     <Image
                         alt={name}
-                        src={photo ? user.photo : defaulImage}
+                        src={photo ? photo : defaultImage}
                         layout='fill'
                     />
                 </div>
             </div>
-
         </header>
     )
 }
